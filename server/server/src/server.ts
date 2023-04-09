82fb2549aa56d39f696392a60af6df5db69f24bf
Marcus Gotling <marcus@gotling.se>
2023-04-09 14:26:21 +0200

    Add logging when starting and connecting

--- server/server/src/server.ts
+++ server/server/src/server.ts
@@ -2,9 +2,10 @@ const mqtt = require('mqtt')
 const client  = mqtt.connect('mqtt://mqtt')
 
 client.on('connect', function () {
+  console.log('Connected!')
   client.subscribe('presence', function (err: object) {
     if (!err) {
-      client.publish('presence', 'Hello mqtt')
+      client.publish('presence', 'Hello mqtt!')
     }
   })
 })
@@ -13,3 +14,5 @@ client.on('message', function (topic: string, message: object) {
   // message is Buffer
   console.log("Received: ", message.toString())
 })
+
+console.log(`Gong server starting.\n\nConnecting to MQTT server..`)

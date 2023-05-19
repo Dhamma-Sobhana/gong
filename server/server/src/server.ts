45fa49c047daf440f26a1cf031a2bc6ef202581a
Marcus Gotling <marcus@gotling.se>
2023-05-19 11:44:41 +0200

    Refactor to object

--- server/server/src/server.ts
+++ server/server/src/server.ts
@@ -1,36 +1,64 @@
 const mqtt = require('mqtt')
 const client  = mqtt.connect('mqtt://mqtt')
-const topics = ["activated", "stop"]
+const topics = ["activated", "played"]
 
-let gongPlaying = false;
+class Server {
+  gongPlaying: boolean = false
 
-client.on('connect', function () {
-  console.log('Connected! Listening for topics:\n', topics.join(', '))
-  for (let topic of topics) {
-    client.subscribe(topic)
+  constructor() {
+    client.on('connect', () => {
+      this.mqttConnect()
+    })
+
+    client.on('message', (topic: string, message: object) => {
+      this.mqttMessage(topic, message)
+    })
   }
-})
 
-function handleRemoteAction() {
-  console.log(`New playing state: ${!gongPlaying}`)
-  if (gongPlaying) {
-    gongPlaying = false
-    client.publish('stop');
-  } else {
-    gongPlaying = true
-    client.publish('play')
+  /**
+   * Subscribe to topics
+   */
+  mqttConnect() {
+    console.log('Connected! Listening for topics:\n', topics.join(', '))
+    for (let topic of topics) {
+      client.subscribe(topic)
+    }
+  }
+
+  /**
+   * Handle subscribed messages received
+   * @param topic MQTT topic
+   * @param message if any, in JSON format
+   */
+  mqttMessage = (topic: string, message: object) => {
+    console.log(`Topic: ${topic} Message: ${message.toString()}`)
+
+    if (topic === 'activated') {
+      this.handleRemoteAction()
+    } else if (topic === 'played') {
+      if (this.gongPlaying == true) {
+        this.gongPlaying = false
+        client.publish('stop')
+      }
+    }
+  }
+
+  /**
+   * Handle remote button press
+   */
+  handleRemoteAction = () => {
+    console.log(`New playing state: ${!this.gongPlaying}`)
+    if (this.gongPlaying) {
+      this.gongPlaying = false
+      client.publish('stop')
+    } else {
+      this.gongPlaying = true
+      client.publish('play')
+    }
   }
 }
 
-client.on('message', function (topic: string, message: object) {
-  // message is Buffer
-  console.log(`Topic: ${topic} Message: ${message.toString()}`)
+// Instantiate server object
+const server = new Server();
 
-  if (topic === 'activated') {
-    handleRemoteAction()
-  } else if (topic === 'stop') {
-    gongPlaying = false
-  }
-})
-
-console.log(`Gong server starting.\n\nConnecting to MQTT server..?`)
+console.log(`Gong server starting.\n\nConnecting to MQTT server..`)

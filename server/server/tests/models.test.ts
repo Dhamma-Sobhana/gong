60b465ce0647a9d15d8345426cacbde04909dbbb
Marcus Gotling <marcus@gotling.se>
2023-08-30 11:01:28 +0200

    Test PlayMessage

--- server/server/tests/models.test.ts
+++ server/server/tests/models.test.ts
@@ -1,6 +1,6 @@
-import { Message } from "../src/models"
+import { Message, PlayMessage } from "../src/models"
 
-test('Instance message', () => {
+test('Instance Message', () => {
     let message = new Message()
 
     expect(message.name).toBe('undefined')
@@ -8,7 +8,7 @@ test('Instance message', () => {
     expect(message.zones).toBe(undefined)
 })
 
-test('Pong remote message', () => {
+test('Pong remote Message', () => {
     let message = Object.assign(new Message(), {"name": "main-building", "type": "remote"})
 
     expect(message.name).toBe('main-building')
@@ -16,10 +16,24 @@ test('Pong remote message', () => {
     expect(message.zones).toBe(undefined)
 })
 
-test('Pong player message', () => {
+test('Pong player Message', () => {
     let message = Object.assign(new Message(), {"name": "male-house", "type": "player", "zones": ["accommodation"]})
 
     expect(message.name).toBe('male-house')
     expect(message.type).toBe('player')
     expect(message.zones).toStrictEqual(["accommodation"])
+})
+
+test('Instance default PlayMessage', () => {
+    let message = new PlayMessage()
+
+    expect(message.zones).toStrictEqual(["all"])
+    expect(message.repeat).toBe(4)
+})
+
+test('Instance custom PlayMessage', () => {
+    let message = new PlayMessage(["accommodation"], 1)
+
+    expect(message.zones).toStrictEqual(["accommodation"])
+    expect(message.repeat).toBe(1)
 })

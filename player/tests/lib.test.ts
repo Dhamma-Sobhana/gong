df75cd0f177e80cba1c4331aeb5942a58cc4b901
Marcus Gotling <marcus@gotling.se>
2023-09-02 21:08:01 +0200

    Add zone test

--- player/tests/lib.test.ts
+++ player/tests/lib.test.ts
@@ -6,4 +6,29 @@ test('get all zones', () => {
 
     let result = getZones(zonesHandled, zonesMessage)
     expect(result).toStrictEqual(['all'])
+
+    zonesMessage = ['female-house']
+    result = getZones(zonesHandled, zonesMessage)
+    expect(result).toStrictEqual(['all'])
+
+    zonesHandled = ['female-house']
+    zonesMessage = ['all']
+    result = getZones(zonesHandled, zonesMessage)
+    expect(result).toStrictEqual(['all'])
+})
+
+test('get selected zones', () => {
+    let zonesHandled = ['female-house', 'male-house']
+    let zonesMessage = ['female-house', 'male-house']
+
+    let result = getZones(zonesHandled, zonesMessage)
+    expect(result).toStrictEqual(['female-house', 'male-house'])
+
+    zonesMessage = ['female-house']
+    result = getZones(zonesHandled, zonesMessage)
+    expect(result).toStrictEqual(['female-house'])
+
+    zonesMessage = ['outside']
+    result = getZones(zonesHandled, zonesMessage)
+    expect(result).toStrictEqual([])
 })

4dd4d1ae064a1991a69eb8ddd665bae31fe476ad
Marcus Gotling <marcus@gotling.se>
2025-04-30 11:59:03 +0200

    Adjust initial setup of MQTT

--- server/mqtt-config/setup.sh
+++ server/mqtt-config/setup.sh
@@ -1,3 +1,5 @@
 cp mosquitto.conf /mosquitto/config
 cp acl_file /mosquitto/config
+cp password_file /mosquitto/config
+chmod 0700 /mosquitto/config/password_file
 ./mosquitto_passwd -b /mosquitto/config/password_file mqtt $MQTT_PASSWORD

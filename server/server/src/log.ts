e5b9013dcceda104eddde3b396ca7263530b0e7d
Marcus Gotling <marcus@gotling.se>
2025-09-25 13:52:53 +0200

    Show the last 100 logs

--- server/server/src/log.ts
+++ server/server/src/log.ts
@@ -1,16 +1,16 @@
-import { DeviceStatus } from "./models"
-
 let logArray: Array<string> = []
+
+const maxLogLines = 100
 
 /**
  * Override console.log to save messages in memory cache and write to console.
- * Limit number of messages to 10
+ * Limit number of messages to maxLogLines
  */
 let _consoleLog = console.log;
 console.log = function () {
     let message = Array.prototype.slice.apply(arguments).join(' ')
 
-    if (logArray.length >= 10)
+    if (logArray.length >= maxLogLines)
         logArray.shift()
 
     let now = new Date().toLocaleString('sv-SE')
--- server/server/tests/log.test.ts
+++ server/server/tests/log.test.ts
@@ -1,15 +1,15 @@
 import { logArray } from "../src/log"
 
-test('memory log maximum 10 entries', () => {
+test('memory log maximum 100 entries', () => {
     expect(logArray.length).toBe(0)
 
-    let repeat = 12
+    let repeat = 102
     while (repeat > 0) {
         console.log("test", repeat)
         repeat--
     }
 
-    expect(logArray.length).toBe(10)
-    expect(logArray[0]).toContain("test 10")
-    expect(logArray[9]).toContain("test 1")
+    expect(logArray.length).toBe(100)
+    expect(logArray[0]).toContain("test 100")
+    expect(logArray[99]).toContain("test 1")
 });
